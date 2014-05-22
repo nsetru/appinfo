fc6bb2f4741e0998a26fd90780ae5bddd3729f37
Nivedita Setru <cceanse@live.ucl.ac.uk>
2014-05-22 15:09:59 +0100

    initial repo setup for mvc

--- library/router.php
+++ library/router.php
@@ -4,3 +4,16 @@
  * This acts as a router file for current app
  */
 
+//TODO::set error reporting here
+function error_reporting(){
+    
+}
+// unregister global variables
+function unregister_globals(){
+    
+}
+
+//includes 
+function __autoload(){
+    
+}
--- public/index.php
+++ public/index.php
@@ -8,5 +8,6 @@ define('ROOT', dirname(dirname(__FILE__)));
 
 $url = isset($_GET['url']) ? $_GET['url'] : NULL;
 
+// define path to config.php and router.php scripts
 require_once (ROOT . DS .'config'. DS . 'config.php');
 require_once (ROOT . DS .'library'. DS . 'router.php');
